ead25cf136ca3b363b67250ca61bed20163d922c
jenilnagrecha <nagrechajenil@gmail.com>
2022-06-29 20:51:07 +0530

    auth backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 console.log('hello world');
+// import lib
 const express = require('express');
+const mongoose = require('mongoose');
+// import pakage
+const authRouter = require("./routes/auth");
+//initialization
 const app = express();
 const PORT =3000;
-
+// mongoose database
+const DB = "mongodb+srv://jenil:qazx@cluster0.ueh7j.mongodb.net/?retryWrites=true&w=majority";
+app.use(express.json());
+app.use(authRouter);
+mongoose.connect(DB).then(()=>{
+    console.log('connection succesfull');
+});
 app.listen(PORT,() => {
-    console.log(`connected at port ${PORT} hello`);
+    console.log(`connected at port ${PORT}`);
 })
